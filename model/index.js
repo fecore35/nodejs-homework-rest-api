9c9ef3ae90499fe0475ea815e904c74df9451ab0
Bryzgun <bryzgun@Evgenii-Bryzgun-MacBook-Pro.local>
2021-12-26 11:48:14 +0200

    Add CRUD Operations

--- model/index.js
+++ model/index.js
@@ -12,7 +12,7 @@ const listContacts = async () => {
 }
 
 const getContactById = async (contactId) => {
-  const [ contact ] = contacts.find((contact) => contact.id === contactId)
+  const contact = contacts.find((contact) => contact.id === contactId)
   return contact
 }
 
@@ -23,8 +23,7 @@ const removeContact = async (contactId) => {
     return null
   }
 
-  const [ result ] = contacts.slice(index, 1)
-
+  const [ result ] = contacts.splice(index, 1)
   await fs.writeFile(
     path.join(__dirname, "contacts.json"),
     JSON.stringify(contacts, null, 2)
@@ -32,32 +31,15 @@ const removeContact = async (contactId) => {
   return result;
 }
 
-const addContact = async (name, email, phone) => {
-  const isName = contacts.find((contact) => contact.name === name)
-  const isEmail = contacts.find((contact) => contact.email === email)
-  const isPhone = contacts.find((contact) => contact.phone === phone)
-
-  if (isName) {
-    return "This name is in the list"
-  }
-
-  if (isEmail) {
-    return "This email is in the list"
-  }
-
-  if (isPhone) {
-    return "This phone is in the list"
-  }
-
-  const newContact = { id: randomUUID(), name, email, phone }
+const addContact = async (body) => {
+  const newContact = { id: randomUUID(), ...body}
 
   contacts.push(newContact)
   await fs.writeFile(
     path.join(__dirname, "contacts.json"),
     JSON.stringify(contacts, null, 2)
   )
-
-  return contacts
+  return newContact
 }
 
 const updateContact = async (contactId, body) => {
@@ -67,7 +49,7 @@ const updateContact = async (contactId, body) => {
     return null
   }
 
-  const updateContact = { id:contactId, ...body }
+  const updateContact = { id:contactId, ...contacts[index], ...body }
   contacts[index] = updateContact
   await fs.writeFile(
     path.join(__dirname, "contacts.json"),
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,52 @@
 import { Router } from 'express' 
+import model from '../../model'
+
 const router = new Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await model.listContacts()
+  res.status(200).json(contacts)
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params
+  const contact = await model.getContactById(id)
+
+  if (contact) {
+    return res.status(200).json(contact)
+  }
+
+  res.status(404).json({ message: 'Not found' })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const isName = 'name' in req.body;
+  const isEmail = 'email' in req.body;
+  const isPhone = 'phone' in req.body;
+  
+  if ( isName && isEmail && isPhone ) {
+    const newContact = await model.addContact(req.body)
+    return res.status(201).json(newContact)
+  }
+  
+  res.status(400).json({"message": "missing required name field"})
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params
+  const contact = await model.removeContact(id)
+
+  if (contact) {
+    return res.status(200).json({"message": "contact deleted"})
+  }
+
+  res.status(404).json({ message: 'Not found' })
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params
+  const updateContact = await model.updateContact(id, req.body)
+  res.status(200).json(updateContact)
 })
 
 export default router
