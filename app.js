b9cb5c50836a578c2100d7a5ca247d7cc3c37cdf
Bryzgun <bryzgun@Evgenii-Bryzgun-MacBook-Pro.local>
2022-01-19 15:18:37 +0200

    Adding limit to request

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short"
 app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: 5000 }))
 
 app.use("/api/auth", authRouter)
 app.use("/api/contacts", contactsRouter)
