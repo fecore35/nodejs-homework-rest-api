1e4bcc65c4fb2d6740a1636267d1662c3943d165
Bryzgun <bryzgun@Evgenii-Bryzgun-MacBook-Pro.local>
2022-01-20 12:16:28 +0200

    Adding serving static files

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json({ limit: 5000 }))
+app.use(express.static("public"))
 
 app.use("/api/auth", authRouter)
 app.use("/api/contacts", contactsRouter)
